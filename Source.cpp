a40476d635bcaef2db4889e8ef68deb9ef558122
kenji <kenji256@gmail.com>
2018-05-11 10:49:03 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,94 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#pragma comment(lib, "Shlwapi.lib")
+
+#include <windows.h>
+#include <Shlwapi.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hEdit;
+	static HWND hButton;
+	static HWND hStatic;
+	static HICON hIcon;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), TEXT("C:\\WINDOWS\\notepad.exe"), WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("取得"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hStatic = CreateWindow(TEXT("STATIC"), 0, WS_VISIBLE | WS_CHILD | SS_ICON | SS_REALSIZECONTROL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit, 10, 10, LOWORD(lParam) - 20, 32, TRUE);
+		MoveWindow(hButton, 10, 50, 256, 32, TRUE);
+		MoveWindow(hStatic, 10, 90, 32, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			if (hIcon)
+			{
+				DestroyIcon(hIcon);
+			}
+			WCHAR szInputString[1024] = { 0 };
+			GetWindowText(hEdit, szInputString, _countof(szInputString));
+			WCHAR szOutString[1024] = { 0 };
+			SHFILEINFO sfi = { 0 };
+			SHGetFileInfo(szInputString, 0, &sfi, sizeof(sfi), SHGFI_ICON | SHGFI_LARGEICON);
+			hIcon = sfi.hIcon;
+			SendMessage(hStatic, STM_SETICON, (WPARAM)hIcon, 0);
+		}
+		break;
+	case WM_DESTROY:
+		if (hIcon)
+		{
+			DestroyIcon(hIcon);
+		}
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("SHGetFileInfo を使ってアイコンを取得"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
